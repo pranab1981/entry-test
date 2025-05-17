150ebdd9ca8865568f3d0482c80c78ebf3c51806
berksunduri <berksunduri@protonmail.com>
2025-05-17 14:57:24 +0200

    Fix login redirect bug and implement Cypress Test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,29 @@
+/* global cy */
+
 describe('Login Component', () => {
-}) 
+  beforeEach(() => {
+    cy.visit('http://localhost:3000'); 
+  });
+
+  // check for login.js
+  it('renders the login form', () => {
+    cy.get('form.login-form').should('exist');
+    cy.get('input#name').should('exist');
+    cy.get('input#password').should('exist');
+    cy.get('button.login-button').should('contain', 'Login');
+  });
+
+  //check for welcome.js
+  it('logs in successfully and redirects to welcome screen', () => {
+    const testName = 'Berk';
+    const testPassword = 'test123';
+    // fill the form
+    cy.get('input#name').type(testName);
+    cy.get('input#password').type(testPassword);
+    cy.get('button.login-button').click();
+
+    // check for welcome msg
+    cy.contains(`Welcome, ${testName}!`).should('exist');
+    cy.get('button').contains('Logout').should('exist');
+  });
+});

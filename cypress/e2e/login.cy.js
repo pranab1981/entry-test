560f2dd010e2736675d704cd9b20907ae0e1b01e
VijaySingh@8650 <rathorevijay995@gmail.com>
2025-05-17 09:56:03 +0530

    bug: redirection-solved & tested also

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,26 @@
-describe('Login Component', () => {
+describe('LoginForm', () => {
+
+    beforeEach(()=>{
+        cy.visit("/");
+    });
+    
+    //while logging-in
+    it('should login with name and password', ()=>{
+       cy.get('input[name="name"]').type('Vijay');
+       cy.get('input[name="password"]').type('vijay1234');
+       cy.get('form').submit();
+
+
+       cy.contains('Welcome').should('exist');
+       cy.contains('Vijay').should('exist');
+
+    });
+
+    //handling errors
+    it('should show errors if input fields are empty', ()=>{
+        cy.get('button[type="submit"]').click();
+        cy.get('input:invalid').should('have.length', 2);
+    })
+
+
 }) 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -14,10 +14,16 @@ function LoginForm({ onLogin }) {
       [name]: value
     }));
   };
+ 
+  //I used this function to handle the onLogin function when submitting the form
+  const handleSubmit = (e) => {
+    e.preventDefault(); //preventing form from refreshing the page
+    onLogin(formData);
+  }
 
   return (
     <div className="login-form-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}> {/* onSubmit event handler was missing, thats why it was not redirecting */}
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="name">Name:</label>

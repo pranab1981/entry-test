42f30dea891b1f720058d88e8755ede1d34bdcf1
Chimaobi <ps3hdguy@gmail.com>
2025-05-17 16:41:13 +0100

    Fixed login bug and added Cypress E2E test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,17 @@
-describe('Login Component', () => {
-}) 
+/* global cy */
+describe("Login Component", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+  it("should fill in the login form and submit", () => {
+    //Fill in loging form
+    cy.get('input[name="name"]').type("testuser");
+    cy.get('input[name="password"]').type("password");
+    // Submit the form
+    cy.get(".login-button").click();
+
+    // Check if Welcome message appears (indicating redirect)
+    cy.contains("Welcome,").should("be.visible");
+    cy.get("button").contains("Logout").should("exist");
+  });
+});

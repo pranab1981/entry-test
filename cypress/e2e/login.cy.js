34729c655717a03aaad55628a1e4177afd65d98d
Akhil <akhilakhi298@gmail.com>
2025-05-18 02:19:44 +0530

    Bug was when clicking submit the formData is not going into the onLogin funtion if this funtin takes the data then it will set the usestate to true then loggedin will be true and the ternary operator renders accordingly

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,43 @@
 describe('Login Component', () => {
-}) 
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should render the login page', () => {
+    cy.contains('Login').should('exist');
+    cy.get('input[name="name"]').should('exist');
+    cy.get('input[name="password"]').should('exist');
+  });
+
+  it('should validate empty fields submission', () => {
+    cy.get('button[type="submit"]').click();
+    cy.get('form.login-form').should('exist');
+    cy.contains('Welcome').should('not.exist');
+  });
+
+  const users = [
+    { name: 'Akhil', password: 'pass123' },
+    { name: 'Nikhil', password: '123@123' },
+    { name: 'TestNewCandidate', password: 'password@2211' }
+  ];
+
+  users.forEach(({ name, password }) => {
+    it(`should log in successfully with user: ${name}`, () => {
+      cy.get('input[name="name"]').type(name);
+      cy.get('input[name="password"]').type(password);
+      cy.get('button[type="submit"]').click();
+
+      cy.contains(`Welcome, ${name}!`).should('exist');
+      cy.contains('Logout').should('exist').click();
+      cy.contains('Login').should('exist');
+    });
+  });
+
+  it('should logout successfully and return to login page', () => {
+    cy.get('input[name="name"]').type('Akhil');
+    cy.get('input[name="password"]').type('pass123');
+    cy.get('button[type="submit"]').click();
+    cy.contains('Logout').click();
+    cy.contains('Login').should('exist');
+  });
+});

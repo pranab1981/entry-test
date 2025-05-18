dee6f0e8379d7265bc2c98d48cd5df0e316cd6e3
VeloraOS <olaiwolaayoola@yahoo.com>
2025-05-18 11:49:56 +0100

    test: make tests reusable

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,5 @@
 describe('Login Flow with Component Switch', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000');
-  });
-
   it('logs in and shows welcome message with username', () => {
-    // Type the name and password
-    cy.get('[data-testid="input-name"]').type('testuser');
-    cy.get('[data-testid="input-password"]').type('anything');
-
-    // Submit the form
-    cy.get('[data-testid="login-button"]').click();
-
-    // Assert that the login form disappears
-    cy.get('[data-testid="login-form"]').should('not.exist');
-
-    // Assert that the welcome message with the username appears
-    cy.contains('Welcome, testuser').should('exist');
+    cy.login('layoolar','password');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... }) 
+Cypress.Commands.add('login', (name = 'testuser', password = 'anything') => {
+  cy.visit('http://localhost:3000');
+  cy.get('[data-testid="input-name"]').type(name);
+  cy.get('[data-testid="input-password"]').type(password);
+  cy.get('[data-testid="login-button"]').click();
+  cy.get('[data-testid="login-form"]').should('not.exist');
+  cy.contains(`Welcome, ${name}`).should('exist');
+});

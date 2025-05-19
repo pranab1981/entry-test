119fb3c4ebda67a3ed5fbb53d50007b94db1d3e5
DianaShulha <dianashulha9@gmail.com>
2025-05-19 20:09:41 +0300

    fix(login): add form submission handler and e2e test with Cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,20 @@
+/* global cy */
+
 describe('Login Component', () => {
+  it('logs in and shows welcome message', () => {
+    cy.visit('/');
+
+    cy.get('input[name="name"]').type('UserName');
+    cy.get('input[name="password"]').type('123456');
+    cy.get('form.login-form').submit();
+
+    cy.contains('Welcome, UserName!').should('be.visible');
+    cy.contains('Logout').should('be.visible');
+
+    cy.get('button.logout-button').click();
+
+    cy.get('form.login-form').should('be.visible');
+    cy.get('input[name="name"]').should('be.visible');
+    cy.get('input[name="password"]').should('be.visible');
+  })
 }) 

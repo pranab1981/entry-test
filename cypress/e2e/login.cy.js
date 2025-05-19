40e88df6eefe158d9860bf7ac59b15424bad5127
VimalKManoj <vimalkmanoj@gmail.com>
2025-05-19 16:56:59 +0530

    fix login handleSubmit and redirect to welcome

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,11 @@
-describe('Login Component', () => {
-}) 
+describe("Login Component", () => {
+  it("should login successfully and redirect to welcome page", () => {
+    cy.visit("/");
+
+    cy.get('input[name="name"]').type("Vimal K Manoj");
+    cy.get('input[name="password"]').type("password123");
+    cy.get('button[type="submit"]').click();
+
+    cy.contains("Welcome, Vimal K Manoj!").should("exist");
+  });
+});

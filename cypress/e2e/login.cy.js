afc45196d8ef8ad695b0368747f3048015fc25ed
Aman_S <100036754+Frenzy117@users.noreply.github.com>
2025-05-19 16:40:16 -0400

    Initial Thoughts

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,15 @@
 describe('Login Component', () => {
+    it('logs in successfully',() => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('input[name="name"]')//get the name input field
+        cy.get('input[name="password"]')//get the password input field
+        cy.get('input[name="name"]').type('testuser')
+        cy.get('input[name="password"]').type('testpassword')// fill both up with user and pass
+        cy.get('button[type="submit"]').click()//click the login button
+        cy.get('.welcome-container').should('exist')//welcome page should be rendered, since this is the class of the main container
+        //didn't really go for getting the url since after doing heavy editting, I figured the URL doesn't change
+        //and that there's no explicit paths set for each of the pages
+
+        //this should be enough for the entire flow.
+    })
 }) 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import './LoginForm.css';
 
 function LoginForm({ onLogin }) {
+
+  //Cypress put the inputs in correctly and clicjed the button. but nothing happened, even through the user information was passed correctly
+  //So far, onLogin is not being used or called.
+  //therefore, no connection between the main App, the Login Form, and the Welcome screens.
+  //<App/> doesn't know we have logged in. So state/ page doesn't change
+
   const [formData, setFormData] = useState({
     name: '',
     password: ''
@@ -17,10 +23,13 @@ function LoginForm({ onLogin }) {
 
   return (
     <div className="login-form-container">
+
+      {/* Got the approach from chatGPT on how basic form submissions are handled*/}
       <form className="login-form">
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="name">Name:</label>
+          {/* tried playing around with the name, 'id' fields to see if the Cypress script and handleChange is getting the values passed correctly */}
           <input
             type="text"
             id="name"
@@ -41,7 +50,7 @@ function LoginForm({ onLogin }) {
             required
           />
         </div>
-        <button type="submit" className="login-button">
+        <button type="submit" className="login-button">{/* no action when clicked??? How is the form submitted then?*/}
           Login
         </button>
       </form>

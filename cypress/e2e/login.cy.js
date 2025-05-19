79e83ffdef9d616f4803e64810616c3297710ec1
MastewalB <se.mastewal.belete@gmail.com>
2025-05-19 16:29:26 +0300

    Login Redirect fix and Cypress E2E test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,13 @@
 describe('Login Component', () => {
+    it('passes', {retries: 1}, () => {
+        cy.visit('http://localhost:3000')
+
+        cy.contains('Login')
+
+        cy.get('input[name="name"]').type('testuser')
+        cy.get('input[name="password"]').type('testpassword')
+        cy.get('button[type="submit"]').click()
+        cy.contains('Welcome, testuser!')
+
+    })
 }) 

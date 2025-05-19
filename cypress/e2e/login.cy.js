301ac44599ac69ce1d3ba469a779f5ea556c9505
Deepcoders30 <umair.khan.rocko@gmail.com>
2025-05-19 22:46:50 +0530

    chore: fix redirect bug and write e2e test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,17 @@
 describe('Login Component', () => {
+    it("first login and redirect to welcome page", () => {
+        cy.visit('http://localhost:3000');
+
+        // FIll login form
+        cy.get('input[name="name"]').type('sampleuser');
+        cy.get('input[name="password"]').type('sampleuser123');
+
+        // Now form submit
+        cy.get('form').submit();
+
+        // check that we're on /welcome
+        cy.url().should('include', '/welcome');
+
+        cy.contains("Welcome, sampleuser!").should('be.visible');
+    })
 }) 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -15,9 +15,15 @@ function LoginForm({ onLogin }) {
     }));
   };
 
+  // There was no onSubmit function inside form component. Now i added it using onLogin prop that LoginForm component received.
+  const handleSubmit = (e) => {
+    e.preventDefault();  // Prevent page reload
+    onLogin(formData)
+  }
+
   return (
     <div className="login-form-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <h2>Login</h2>
         <div className="form-group">
           <label htmlFor="name">Name:</label>

78051dcb931120a92bebe5717e8f2d40c4d25c1d
owl099 <alokk.maurya21@gmail.com>
2025-05-20 23:09:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             )
           }
         />
-        {/* Redirect any unknown route to login */}
+        {/* Redirecting  any unknown route to login */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>

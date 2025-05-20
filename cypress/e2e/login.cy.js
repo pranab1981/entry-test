08bb6f99078c783f0ee0da0e4012d6eb0896233d
Sule Bello <sbello1234@gmail.com>
2025-05-19 20:54:22 -0400

    login_test: prevent form submission with empty input fields

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,6 +8,18 @@ describe('Login Component', () => {
         cy.get('input[name="password"]').should('exist');
         cy.get('button[type="submit"]').should('exist');
 
+      
+    });
+    it('prevents submission when input fields are empty', () => {
+        cy.get('button[type="submit"]').click();
+        cy.get('input[name="name"]').then($input => {
+            expect($input[0].checkValidity()).to.be.false;
+          });
+
+        cy.get('input[name="password"]').then($input => {
+            expect($input[0].checkValidity()).to.be.false;
+          });
+
     });
 
 }) 

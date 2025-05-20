02d9b0d6902b170abb34e36aef8e364e6419cc2b
Sule Bello <sbello1234@gmail.com>
2025-05-19 20:49:25 -0400

    login_test: verify rendering of name, password inputs, and submit button

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,13 @@
 describe('Login Component', () => {
+    beforeEach(() => {
+        cy.visit('/');
+      });
+
+      it('should render name and password inputs and login button', () => {
+        cy.get('input[name="name"]').should('exist');
+        cy.get('input[name="password"]').should('exist');
+        cy.get('button[type="submit"]').should('exist');
+
+    });
+
 }) 

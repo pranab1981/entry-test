e21dbad876f89be470538ecfe5b32dbaf9c61e44
owl099 <alokk.maurya21@gmail.com>
2025-05-20 23:10:03 +0530

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -17,7 +17,7 @@ function LoginForm({ onLogin }) {
 
   const handleSubmit = (e) => {
     e.preventDefault(); // prevent page reload
-    // You can add validation here if needed
+   
     onLogin(formData); // pass login data to parent
   };
 

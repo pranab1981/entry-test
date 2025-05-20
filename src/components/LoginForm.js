51a2b14c59f6a4807c779a2397fee5f0a61678ac
justjeevs <58017609+justjeevs@users.noreply.github.com>
2025-05-20 19:30:59 -0400

    added onSubmit event handler for login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,51 +2,56 @@ import React, { useState } from 'react';
 import './LoginForm.css';
 
 function LoginForm({ onLogin }) {
-  const [formData, setFormData] = useState({
-    name: '',
-    password: ''
-  });
+    const [formData, setFormData] = useState({
+        name: '',
+        password: '',
+    });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
 
-  return (
-    <div className="login-form-container">
-      <form className="login-form">
-        <h2>Login</h2>
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onLogin(formData);
+    };
+
+    return (
+        <div className='login-form-container'>
+            <form className='login-form' onSubmit={handleSubmit}>
+                <h2>Login</h2>
+                <div className='form-group'>
+                    <label htmlFor='name'>Name:</label>
+                    <input
+                        type='text'
+                        id='name'
+                        name='name'
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password'>Password:</label>
+                    <input
+                        type='password'
+                        id='password'
+                        name='password'
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <button type='submit' className='login-button'>
+                    Login
+                </button>
+            </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" className="login-button">
-          Login
-        </button>
-      </form>
-    </div>
-  );
+    );
 }
 
-export default LoginForm; 
+export default LoginForm;
